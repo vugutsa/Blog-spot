250fd7d9c1ce88501637814839a7abe36a031130
vugutsa <vugutsamercy84@gmail.com>
2020-12-14 10:51:57 +0300

    create a function of the running the unit tests in the manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app,db
-from app.models import User,Role,Comments
+from app.models import User,Role,Comments,Blog
 from flask_script import Manager, Server
 from  flask_migrate import Migrate, MigrateCommand
 
@@ -14,12 +14,12 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-# @manager.command
-# def test():
-#     """Run the unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
