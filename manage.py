f2e80febaffbeddd89dca02005209c2572ea82b4
vugutsa <vugutsamercy84@gmail.com>
2020-12-14 10:50:50 +0300

    create an instance of the app

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager, Server
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('test')
+# app = create_app('test')
 app = create_app('development')
 
 
@@ -14,12 +14,12 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-# export MAIL_USERNAME=vugutsamercy84@gmail.com
-# export MAIL_PASSWORD=35139931v@
-# export SECRET_KEY='351399'
+export MAIL_USERNAME=vugutsamercy84@gmail.com
+export MAIL_PASSWORD=35139931v@
+export SECRET_KEY='351399'
 
-# python3.6 manage.py server
+python3.6 manage.py server
